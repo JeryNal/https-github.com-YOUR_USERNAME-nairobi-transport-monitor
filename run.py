0acbafe079007e9e3b3b24b1bb935cbbe8ae94d6
coded <wamalwagerald98@gmail.com>
2026-07-24 13:11:44 -0400

    Add Docker support for Flask application

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "").lower() in {"1", "true", "yes"}
     socketio.run(
         app,
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=5000,
         debug=debug,
         use_reloader=False,
